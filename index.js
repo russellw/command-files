a5cd9e8e7a43fbcb8003f12fb3f70eb5182e417a
Russell Wallace <russell.wallace@gmail.com>
2017-04-03 01:20:35 +0100

    response files

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 'use strict';
+var fs = require('fs');
 var glob = require('glob');
 var os = require('os');
+
+function response(arg, files) {
+	var file = arg.slice(1);
+	var text = fs.readFileSync(file, {
+		encoding: 'utf8',
+	});
+	var lines = text.split(/\r?\n/);
+	for (var line of lines) {
+		if (line[0] === '@') {
+			response(line, files);
+			continue;
+		}
+		files.push(line);
+	}
+}
 
 // API
 
 function expand(args) {
+
+	// - means stdin
+	if (args.length === 1 && args[0] === '-') {
+		return [];
+	}
+
+	// Glob
 	var files = args;
 	if (os.platform() === 'win32') {
 		files = [];
@@ -17,8 +40,16 @@ function expand(args) {
 			}
 		}
 	}
-	if (files.length === 1 && files[0] === '-') {
-		files = [];
+
+	// Response files
+	var lines = files;
+	files = [];
+	for (var line of lines) {
+		if (line[0] === '@') {
+			response(line, files);
+			continue;
+		}
+		files.push(line);
 	}
 	return files;
 }
